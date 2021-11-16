fea5b21766eab7d451a456f013f3bce68c09e833
misterdepth <62363912+misterdepth@users.noreply.github.com>
2021-11-15 20:26:26 -0800

    secret stuff p[art 2222

---
+++ special-resources/password.js
@@ -0,0 +1,9 @@
+function letThrough(password) {
+    if(password == "yourmother") {
+        window.location.href="./access.html"
+    } else if(password == "sex" || password == "69") {
+        alert("nice, but that isnt the password lmao")
+    } else {
+        alert("Login Failed")
+    }
+}
